4f26da5755c9320841b4714ec53840d56e800386
sakuxz <yui12327@gmail.com>
2016-06-02 16:10:02 +0800

    zip jpg and add expend-btn

--- public/assets/myjs/index.jsx
+++ public/assets/myjs/index.jsx
@@ -2,25 +2,36 @@ require('index.scss');
 $(function () {
   $('[data-rel^=lightcase]').lightcase();
   $('.expend-btn').each(function(){
-    $(this).data("h", $("#"+$(this).data("id"))[0].offsetHeight );
-    $("#"+$(this).data("id")).css({
-      height: 0,
-      opacity: 0
+    $("img" ,"#"+$(this).data("id")).each(function(i,e) {
+      //$(this).css("transition-delay",i*0.1 + "s");
     });
     $(this).click(function() {
-      if($("#"+$(this).data("id")).css("height") === '0px'){
-        $("#"+$(this).data("id")).css({
-          height: $(this).data("h"),
-          opacity: 1
-        });
-      }else{
-        $("#"+$(this).data("id")).css({
-          height: 0,
-          opacity: 0
-        });
-      }
+       $("#"+$(this).data("id")).toggleClass("vis");
     });
   });
 
-  
 });
+
+// onload = function(){
+//   $('.expend-btn').each(function(){
+//     $(this).data("h", $("#"+$(this).data("id"))[0].offsetHeight );
+//     console.log($("#"+$(this).data("id"))[0].offsetHeight);
+//     $("#"+$(this).data("id")).css({
+//       height: 0,
+//       opacity: 0
+//     });
+//     $(this).click(function() {
+//       if($("#"+$(this).data("id")).css("height") !==  $(this).data("h")+"px" ){
+//         $("#"+$(this).data("id")).css({
+//           height: $(this).data("h"),
+//           opacity: 1
+//         });
+//       }else{
+//         $("#"+$(this).data("id")).css({
+//           height: 0,
+//           opacity: 0
+//         });
+//       }
+//     });
+//   });
+// };
